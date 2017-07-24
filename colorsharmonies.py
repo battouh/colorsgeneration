c3bd5da94f5fe574df07ca2406116bc57a7728fa
Baptiste Manteau <baptiste.manteau@allegorithmic.com>
2017-07-24 17:43:16 +0200

    Color harmonies: handle tetradic harmonies

--- colorsharmonies.py
+++ colorsharmonies.py
@@ -33,7 +33,6 @@ def triadicColor(ColorInput):
 	return [ColorOutput1.RGB,ColorOutput2.RGB]
 
 def splitComplementaryColor(ColorInput):
-
 	# Convert RGB (base 256) to HLS (between 0 and 1 )
 	ColorInput.HLS = list(colorsys.rgb_to_hls(ColorInput.RGB[0] / 255, ColorInput.RGB[1] / 255, ColorInput.RGB[2] / 255))
 
@@ -50,3 +49,28 @@ def splitComplementaryColor(ColorInput):
 	ColorOutput2.RGB = list(map(lambda x: round(x * 255),colorsys.hls_to_rgb(ColorOutput2.HLS[0],ColorOutput2.HLS[1],ColorOutput2.HLS[2])))
 
 	return [ColorOutput1.RGB,ColorOutput2.RGB]
+
+def tetradicColor(ColorInput):
+
+	# Convert RGB (base 256) to HLS (between 0 and 1 )
+	ColorInput.HLS = list(colorsys.rgb_to_hls(ColorInput.RGB[0] / 255, ColorInput.RGB[1] / 255, ColorInput.RGB[2] / 255))
+
+	# Find the first tetradic Hue
+	FirstTetradicHue = ((ColorInput.HLS[0] * 360 + 60) % 360) / 360
+
+	# Find the second tetradic Hue
+	SecondTetradicHue = ((ColorInput.HLS[0] * 360 + 180) % 360) / 360
+
+	# Find the third tetradic Hue
+	ThirdTetradicHue = ((ColorInput.HLS[0] * 360 + 240) % 360) / 360
+
+
+	ColorOutput1 = Color("",[FirstTetradicHue,ColorInput.HLS[1],ColorInput.HLS[2]],"")
+	ColorOutput2 = Color("",[SecondTetradicHue,ColorInput.HLS[1],ColorInput.HLS[2]],"")
+	ColorOutput3 = Color("",[ThirdTetradicHue,ColorInput.HLS[1],ColorInput.HLS[2]],"")
+
+	ColorOutput1.RGB = list(map(lambda x: round(x * 255),colorsys.hls_to_rgb(ColorOutput1.HLS[0],ColorOutput1.HLS[1],ColorOutput1.HLS[2])))
+	ColorOutput2.RGB = list(map(lambda x: round(x * 255),colorsys.hls_to_rgb(ColorOutput2.HLS[0],ColorOutput2.HLS[1],ColorOutput2.HLS[2])))
+	ColorOutput3.RGB = list(map(lambda x: round(x * 255),colorsys.hls_to_rgb(ColorOutput3.HLS[0],ColorOutput3.HLS[1],ColorOutput3.HLS[2])))
+
+	return [ColorOutput1.RGB,ColorOutput2.RGB,ColorOutput3.RGB]
--- test_colorsharmonies.py
+++ test_colorsharmonies.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import unittest
 from colorvariation import Color
-from colorsharmonies import complementaryColor, triadicColor, splitComplementaryColor
+from colorsharmonies import complementaryColor, triadicColor, splitComplementaryColor, tetradicColor
 
 # Obtain the complementary color of a color
 
@@ -43,6 +43,20 @@ class colorsHarmonies(unittest.TestCase):
 		self.assertEqual(sorted(splitComplementaryColor(PurpleColor)),sorted([[170,255,180],[245,255,170]]))
 		self.assertEqual(sorted(splitComplementaryColor(GreenColor)),sorted([[66,94,230],[202,66,230]]))
 		self.assertEqual(sorted(splitComplementaryColor(WhiteColor)),sorted([[255,255,255],[255,255,255]]))
+
+	def test_tetradicColor(self):
+		MagentaColor = Color([255,0,255],"","")
+		YellowColor = Color([255,255,0],"","")
+		CyanColor = Color([0,255,255],"","")
+		PurpleColor = Color([222,170,255],"","")
+		GreenColor = Color([176,230,66],"","")
+		WhiteColor = Color([255,255,255],"","")
+		self.assertEqual(sorted(tetradicColor(MagentaColor)),sorted([[0,255,0],[0,255,255],[255,0,0]]))
+		self.assertEqual(sorted(tetradicColor(YellowColor)),sorted([[0,0,255],[255,0,255],[0,255,0]]))
+		self.assertEqual(sorted(tetradicColor(CyanColor)),sorted([[255,0,0],[255,255,0],[0,0,255]]))
+		self.assertEqual(sorted(tetradicColor(PurpleColor)),sorted([[203,255,170],[170,255,222],[255,170,203]]))
+		self.assertEqual(sorted(tetradicColor(GreenColor)),sorted([[120,66,230],[230,66,176],[66,230,120]]))
+		self.assertEqual(sorted(tetradicColor(WhiteColor)),sorted([[255,255,255],[255,255,255],[255,255,255]]))
 
 if __name__ == '__main__':
     unittest.main()
